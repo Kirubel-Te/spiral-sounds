18d2bc537e60ee6efb3382b562d709226332fe1f
Kirubel Tesfaye <kirubeltesfaye519@gmail.com>
2026-02-22 19:01:54 +0300

    search and filter properties added

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,11 +15,27 @@ export async function getGenres(req, res) {
 export async function getProducts(req, res) {
     try{
         const db = await getDBConnection()
-        let query = 'SELECT * FROM products'
-
-        const products = await db.all(query)
-        await db.close()
-        res.json(products)
+        if(req.query.genre){
+            const genre = req.query.genre
+            let query = 'SELECT * FROM products WHERE genre = ?'
+            const param = [genre]
+            const products = await db.all(query, param)
+            await db.close()
+            return res.json(products)
+        }else if(req.query.search){
+            const search = `%${req.query.search}%`
+            let query = 'SELECT * FROM products WHERE title LIKE ? OR artist LIKE ? OR genre LIKE ?'
+            const param = [search, search, search]
+            const products = await db.all(query, param)
+            await db.close()
+            return res.json(products)
+        }
+        else{
+            const products = await db.all('SELECT * FROM products')
+            await db.close()
+            return res.json(products)
+        }
+        
 
     }catch(error){
         console.error('Error fetching products:', error)
