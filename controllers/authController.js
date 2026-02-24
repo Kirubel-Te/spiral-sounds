ef6d72a5f9bfb1708989e2e17c7924cdeeec3365
Kirubel Tesfaye <kirubeltesfaye519@gmail.com>
2026-02-24 17:38:04 +0300

    user validation functionality and add user to database functionality added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,43 @@
+import validator from 'validator'
+import { getDBConnection } from '../db/db.js'
+
 export async function registerUser(req,res){
-    console.log('Registering user with data:', req.body)
+    let {name,email, username, password} = req.body
+    
+
+    if(!name || !email || !username || !password){
+        return res.status(400).json({error: 'All fields are required'})
+    }
+    name = name.trim()
+    email = email.trim()
+    username = username.trim()
+    password = password.trim()
+
+    const regex = /^[a-zA-Z0-9_-]{1,20}$/
+    if (!regex.test(username)) {
+        return res.status(400).json({error: 'Username is invalid'})
+    }
+
+    if(!validator.isEmail(email)){
+        return res.status(400).json({error: 'Email is invalid'})
+    }
+
+    try{
+        const db = await getDBConnection()
+        const existingUser = await db.get('SELECT * FROM users WHERE username = ? OR email = ?', [username, email])
+        if(existingUser){
+            await db.close()
+            return res.status(400).json({error: 'Username or email already exists'})
+        }else{
+            await db.run('INSERT INTO users (name, email, username, password) VALUES (?, ?, ?, ?)', [name, email, username, password])
+            await db.close()
+            return res.status(201).json({message: 'User registered successfully'})
+        }
+
+    }catch(error){
+        console.error('Error registering user:', error)
+        res.status(500).json({error: 'Registration failed. Please try again.'})
+    }
+
+    
 }
